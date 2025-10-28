a8b0309b6b22e51cefad5201ff66cb6da6f701ca
redccc1234 <redccc1234@gamil.com>
2025-10-28 17:19:38 +0900

    JDBC 개요

--- jdbc1/src/edu/kh/jdbc/JDBCExample6.java
+++ jdbc1/src/edu/kh/jdbc/JDBCExample6.java
@@ -1,5 +1,10 @@
 package edu.kh.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Scanner;
+
 public class JDBCExample6 {
 
 	public static void main(String[] args) {
@@ -11,7 +16,72 @@ public class JDBCExample6 {
 		// 성공 시 "수정 성공 !!"
 		// 수정 실패 시 "아이디 또는 비밀번호 불일치" 출력
 		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
 		
+		Scanner sc = null;
+		
+		try {
+			
+
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			String url = "jdbc:oracle:thin:@localhost:1521:XE"; 
+			String userName = "kh_cya";
+			String password = "kh1234";
+			
+			
+			conn = DriverManager.getConnection(url, userName, password);
+			
+			sc = new Scanner(System.in);
+			
+			System.out.print("아이디 : ");
+			String id = sc.next();
+			
+			System.out.print("비밀번호 : ");
+			String pw = sc.next();
+			
+			System.out.print("이름 : ");
+			String name = sc.next();
+			
+			String sql = """
+					     UPDATE TB_USER
+					     SET USER_NAME = ? 
+					     WHERE USER_ID = ? 
+					     AND USER_PW = ?""";            ;
+					
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, name);
+			pstmt.setString(2, id);
+			pstmt.setString(3, pw);
+		
+			conn.setAutoCommit(false);
+			
+			int result =  pstmt.executeUpdate();
+			
+			if(result > 0) { // UPDATE 성공 시
+				conn.commit(); // COMMIT 수행 -> DB에 INSERT 데이터 영구 반영
+			    System.out.println("수정 성공 !!");
+								
+				} else { // 실패
+				 conn.rollback(); // 실패 시 ROLLBACK
+				System.out.println( "아이디 또는 비밀번호 불일치");
+								
+			    }
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(pstmt != null)pstmt.close();
+				if(conn!= null)conn.close();
+				if(sc!= null)sc.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 		
 	}
 
